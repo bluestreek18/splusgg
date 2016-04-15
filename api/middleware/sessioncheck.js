7f43eacd4a1729fa04d96601d94f9a22dfa33f91
bluestreek18 <bluestreek@gmail.com>
2016-04-14 22:47:27 -0500

    Fixed bug when no referrer!

--- api/middleware/sessioncheck.js
+++ api/middleware/sessioncheck.js
@@ -2,11 +2,16 @@ var geoip = require('geoip-lite');
 
 
 module.exports = function(req, res, next) {
-	var ref = req.get('Referrer').slice(0, 16) === 'https://splus.gg';
+	var ref = req.get('Referrer');
+	if(ref) {
+		ref = ref.slice(0, 16) === 'https://splus.gg';
+	} else {
+		ref = false;
+	}
+
 	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 	var userAgent = req.headers['user-agent'];
 	var geo = geoip.lookup(ip);
-
 
 	req.locals.store.get(req.sessionID, function(error, sess) {
 		console.log('referrer = ', ref);
