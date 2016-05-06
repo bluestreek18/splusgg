7cdfce2ecf38e73a965d0e056444ff83b526ab66
bluestreek18 <bluestreek@gmail.com>
2016-05-05 23:02:49 -0500

    Added Loading page.

--- client/app.js
+++ client/app.js
@@ -25,3 +25,16 @@ angular.module('splus', [
   });
 
 }])
+.run(function ($rootScope, $location) {
+  $rootScope.$on("$locationChangeStart",function(event, next, current) {
+    console.log('next == ', next, typeof next, next === 'http://localhost:3005/#/teams')
+    if(next === 'http://localhost:3005/#/teams') {
+      console.log('Activating Please Wait!')
+      window.loading_screen = window.pleaseWait({
+        logo: "/img/logo.png",
+        backgroundColor: '#3380ff',
+        loadingHtml: "<p class='loading-message'>Loading Match Data From Rito Games API</p><br><div class='sk-cube-grid'><div class='sk-cube sk-cube1'></div><div class='sk-cube sk-cube2'></div><div class='sk-cube sk-cube3'></div><div class='sk-cube sk-cube4'></div><div class='sk-cube sk-cube5'></div><div class='sk-cube sk-cube6'></div><div class='sk-cube sk-cube7'></div><div class='sk-cube sk-cube8'></div><div class='sk-cube sk-cube9'></div></div>"
+      });
+    }
+  });
+});
